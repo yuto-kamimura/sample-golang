55ea75164ee01d38b65b77bad45cdc5022c7c436
yu-kamimura <s6156@enecom-th.jp>
2023-11-16 14:56:26 +0900

    itemを複数登録、取得できるように修正

--- sample_api/entity/item.go
+++ sample_api/entity/item.go
@@ -1,11 +1,34 @@
 package entity
 
-import "time"
+import (
+	"sampleApi/db"
+	"time"
+)
 
 type (
 	Item struct {
-		ID        int       `gorm:"primarykey;autoIncrement"`
-		Name      string    `gorm:"not null"`
-		CreatedAt time.Time `gorm:"not null"`
+		ID          int       `gorm:"primarykey;autoIncrement"`
+		Name        string    `gorm:"not null"`
+		Description string    `gorm:"not null"`
+		CreatedAt   time.Time `gorm:"not null"`
 	}
+	Items []Item
 )
+
+// 商品を登録する
+func (u *Items) CreateItems() error {
+	dbIsntance := db.GetDB()
+	if err := dbIsntance.Create(&u).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// 商品IDで商品を取得する
+func (u *Items) SelectItemsByItemID(id []int) error {
+	dbInstance := db.GetDB()
+	if err := dbInstance.Where("id IN ?", id).Find(&u).Error; err != nil {
+		return err
+	}
+	return nil
+}
